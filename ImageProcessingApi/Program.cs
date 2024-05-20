a2c7637d2674fe45e735944d01755e59f997098e
DinkarChoudhary <dinkarchoudhary21@gmail.com>
2024-05-21 01:19:33 +0530

    Cors issue fix

--- ImageProcessingApi/Program.cs
+++ ImageProcessingApi/Program.cs
@@ -11,6 +11,15 @@ builder.Services.AddSingleton<IPluginManager, PluginManager>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ImageProcessingApi", Version = "v1" });
@@ -31,7 +40,9 @@ app.UseHttpsRedirection();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImageProcessingApi v1"));
 //
-
+app.UseCors("AllowAll");
+app.UseRouting();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 app.UseAuthorization();
 
 app.MapControllers();
